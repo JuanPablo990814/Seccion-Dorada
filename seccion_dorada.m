12572db672d0912d5c7bf5d2e7c9acf488d4e5e1
Juan Pablo Patiño Bedoya <49729106+JuanPablo990814@users.noreply.github.com>
2020-11-22 22:30:53 -0500

    Add files via upload

---
+++ seccion_dorada.m
@@ -0,0 +1,38 @@
+﻿#iniciar excepcion con valor mayor a 1
+excepcion=1;
+#tolerancia al gusto del ejercicio o en caso de no tener colocar 0
+tolerancia=0.001
+#x inferior
+xl=input('ingrese el x inferior');
+#x superior
+xu=input('ingrese el x superior');
+#funcion
+f=input('ingrese la  función');
+k=1;
+while excepcion>tolerancia
+    #obtener distancias a y b
+    a=0.618*(xu-xl);
+    b=0.618^2*(xu-xl);
+    #obtener x1 y x2
+    x1(k)=xl+a;
+    x2(k)=xl+b;
+    #evaluar en f(x) a x1 y x2
+    x=x1(k);
+    fx1(k)=eval(f);
+    x=x2(k);
+    fx2(k)=eval(f);
+    excepcion=abs(x2(k)-x1(k));
+    #imprimir
+    X = sprintf('k:(%d) xl:(%d) xu:(%d) a:(%d) b:(%d) x1:(%d) x2:(%d) f(x1):(%d) f(x2):(%d) excepcion:(%d)',k,xl,xu,a,b,x1(k),x2(k),fx1(k),fx2(k),excepcion);
+    disp(X)
+    #condicional para continuar
+    if(fx1(k)>fx2(k))
+        xl=x2(k);
+        xu=xu;
+    else
+        xl=xl;
+        xu=x1(k);
+    end
+    disp('--------------------------')
+    k=k+1;
+end
---
+++ seccion_dorada_algoritmo.m
@@ -0,0 +1,53 @@
+﻿#variable de condicional
+excepcion=1;
+
+#tolerancia en caso de usar el while
+tolerancia=0.001
+
+#iteraciones en caso de usar el for en vez del while
+#iteraciones=12;
+
+#xbaja
+#    x----------------------
+xl=0;
+
+#xsuperior
+#   ------------------------x
+xu=3;
+
+#funcion
+f='-(x-2)^2+6';
+
+#cambiar al for en caso de realizarlo por numero de iteraciones
+k=1
+while excepcion>tolerancia
+#for k=1 : iteraciones
+    #obtener distancias a y b
+    a=0.618*(xu-xl);
+    b=0.618^2*(xu-xl);
+    #obtener x1 y x2
+    x1(k)=xl+a;
+    x2(k)=xl+b;
+    #evaluar en f(x) a x1 y x2
+    x=x1(k);
+    fx1(k)=eval(f);
+    x=x2(k);
+    fx2(k)=eval(f);
+    excepcion=abs(x2(k)-x1(k));
+    #imprimir en pantalla
+    X = sprintf('k:(%d) xl:(%d) xu:(%d) a:(%d) b:(%d) x1:(%d) x2:(%d) f(x1):(%d) f(x2):(%d) excepcion:(%d)',k,xl,xu,a,b,x1(k),x2(k),fx1(k),fx2(k),excepcion);
+    disp(X)
+    if(fx1(k)>fx2(k))
+        xl=x2(k);
+        xu=xu;
+    else
+        xl=xl;
+        xu=x1(k);
+    end
+    disp('--------------------------')
+    k=k+1;;
+end
+
+
+    
+
